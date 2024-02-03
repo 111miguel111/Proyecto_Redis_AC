8bb25b13c4603653d57a376108c53ae7b79f0a9d
RCast23 <RobertoCastillaSenero@gmail.com>
2024-02-03 23:19:11 +0100

    Ya se como funcionan los constructores
    Mira test para mas referencia

--- ProyectoRedis/src/Controller/Test.py
+++ ProyectoRedis/src/Controller/Test.py
@@ -1,6 +1,8 @@
 from ProyectoRedis.src.Model import Arma
+from ProyectoRedis.src.Model import Cuerpo
+from ProyectoRedis.src.Model import Pieza
 
-armaPrueba = {
+armaPrueba = {#Aqui metemos en un diccionario los datos
     "nombre":input("Introduzca el nombre"),
     "tipoDamage":input("Introduzca el tipo de damage"),
     "dps":input("Introduzca el damage por segundo"),
@@ -10,6 +12,29 @@ armaPrueba = {
     "precio":input("Introduzca el precio del arma")
 }
 
-a = Arma(armaPrueba)
+a = Arma.Arma(armaPrueba)#Para construir el objeto por alguna razon tienes que usar un metodo que se llama igual que la clase de esta forma
 
-print(a)
+cuerpoPrueba = {#Aqui metemos en un diccionario los datos
+    "nombre":input("Introduzca el nombre"),
+    "cabeza":input("Introduzca el modelo de cabeza"),
+    "cuerpo":input("Introduzca el modelo del cuerpo"),
+    "brazos":input("Introduzca el modelo de los brazos"),
+    "piernas":input("Introduzca el modelo de las piernas"),
+    "armaBDer":input("Introduzca el modelo de arma del brazo derecho"),
+    "armaBIzq":input("Introduzca el modelo de arma del brazo izquierdo"),
+    "armaHDer":input("Introduzca el modelo de arma del hombro derecho"),
+    "armaHIzq":input("Introduzca el modelo de arma del hombro izquierdo")
+}
+
+b = Cuerpo.Cuerpo(cuerpoPrueba)
+
+piezaPrueba = {#Aqui metemos en un diccionario los datos
+    "nombre":input("Introduzca el nombre"),
+    "tipoPieza":input("Introduzca el tipo de la pieza"),
+    "armadura":input("Introduzca el valor de armadura"),
+    "consumoEnergia":input("Introduzca el valor de consumo de energia"),
+    "peso":input("Introduzca el peso de la pieza"),
+    "precio":input("Introduzca el precio de la pieza")
+}
+
+c = Pieza.Pieza(piezaPrueba)
